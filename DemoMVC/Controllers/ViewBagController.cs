dae42fb012c0e0cb904363bdbecf0e179c734064
tram14012004 <tram14012004@gmail.com>
2026-01-27 08:02:10 +0700

    update DemoMVC

--- DemoMVC/Controllers/ViewBagController.cs
+++ DemoMVC/Controllers/ViewBagController.cs
@@ -1,9 +1,33 @@
-public class ViewBagController : Controller
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers;
+
+public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
     public IActionResult Vidu()
     {
         ViewBag.Message = "Xin chào, đây là trang Home";
         ViewBag.Year = 2026;
         return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
