156e2e1eb32998bf67f96d96eae9610afbc17ee4
tram14012004 <tram14012004@gmail.com>
2026-01-27 07:52:40 +0700

    update DemoMVC

--- DemoMVC/Controllers/ViduViewBagController.cs
+++ DemoMVC/Controllers/ViduViewBagController.cs
@@ -1,30 +1,12 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using DemoMVC.Models;
-
-namespace DemoMVC.Controllers;
 
 public class ViduViewBagController : Controller
 {
-    private readonly ILogger<ViduViewBagController> _logger;
-
-    public ViduViewBagController(ILogger<ViduViewBagController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Vidu()
     {
-        ViewBag.Message = "Xin chào, đây là trang của Trâm";
-        ViewBag.Year = 2004;
+        ViewBag.Message = "Xin chào, đây là trang Home";
+        ViewBag.Year = 2026;
 
         return View();
-    }
-
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
