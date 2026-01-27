2c205f80a73238bd512d5af3744f500312450df6
tram14012004 <tram14012004@gmail.com>
2026-01-27 07:57:42 +0700

    update DemoMVC

--- DemoMVC/Controllers/ViewBagController.cs
+++ DemoMVC/Controllers/ViewBagController.cs
@@ -1,12 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
-
 public class ViewBagController : Controller
 {
     public IActionResult Vidu()
     {
         ViewBag.Message = "Xin chào, đây là trang Home";
         ViewBag.Year = 2026;
-
         return View();
     }
 }
